4ef1ef99b2b0c0ebfc2a2e4151108d323d47cbcb
moonway <59399356+moonway-dev@users.noreply.github.com>
2023-01-27 06:20:19 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <img src="https://remnant.screwltd.com/img/top-logo@2x.png"/>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
