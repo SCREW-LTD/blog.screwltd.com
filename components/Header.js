d4fd051e34521cb3d00dc8e5f3f04e0e4f67185a
четырка <fpozitiv3@gmail.com>
2023-01-27 07:11:27 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,7 @@ const styles = {
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className={styles.center}>
-        <img src="https://raw.githubusercontent.com/moonway-dev/screw-blog/main/components/logo.png" />
-      </div>
+      <img className={styles.center} src="https://raw.githubusercontent.com/moonway-dev/screw-blog/main/components/logo.png" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
