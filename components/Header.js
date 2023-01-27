f9f9da33c40d3b804fe5b98e56f69222580e152e
четырка <fpozitiv3@gmail.com>
2023-01-27 06:31:07 +0300

    logo

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src="https://screwltd.com/img/screw---------1@2x.png"/>
+      <img src="../components/logo.png"/>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
