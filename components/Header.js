c8dd610b3982be037d11bd843116adba93a9ce0d
четырка <fpozitiv3@gmail.com>
2023-01-27 07:24:52 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -16,6 +16,7 @@ export default function Header({ name }) {
       <style jsx>{`
         .logo-screw {
           margin: 0 auto;
+          padding-bottom: 5px;
         }
       `}</style>
     </header>
