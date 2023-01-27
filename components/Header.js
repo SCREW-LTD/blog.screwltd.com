7899690c841f54b5b915324c28fee03a4817e270
четырка <fpozitiv3@gmail.com>
2023-01-27 07:09:45 +0300

    wqwww

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 import Link from 'next/link';
 
+const styles = {
+  center: {
+    margin: "0 auto"
+  }
+}
+
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
+      <div className={styles.center}>
+        <img src="https://raw.githubusercontent.com/moonway-dev/screw-blog/main/components/logo.png" />
+      </div>
       <p className="text-2xl dark:text-white text-center">
-      <img src="https://raw.githubusercontent.com/moonway-dev/screw-blog/main/components/logo.png" />
         <Link href="/">
           <a>{name}</a>
         </Link>
