b831df794bc174e2b0f724aebc718e6ea65f5a97
четырка <fpozitiv3@gmail.com>
2023-01-27 07:17:48 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img className="justify-content-center" src="https://raw.githubusercontent.com/moonway-dev/screw-blog/main/components/logo.png" />
+      <img className="center" src="https://raw.githubusercontent.com/moonway-dev/screw-blog/main/components/logo.png" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
