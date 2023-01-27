b99884e8f009f8bcd852a08ecc990bd74a123325
четырка <fpozitiv3@gmail.com>
2023-01-27 06:32:53 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src="../components/logo.png"/>
+      <Link href="/">
+        <img src="../components/logo.png" />
+      </Link>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
